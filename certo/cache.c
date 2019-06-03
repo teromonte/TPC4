c195c1b7698aa56d1410d594675ab10e32865ad0
Thiago <thiago@brasil247.com.br>
2019-06-03 14:51:53 +0100

    100 no moosak

--- certo/cache.c
+++ certo/cache.c
@@ -22,123 +22,193 @@ unsigned Cvalid[SETS][LINESPERSET];  // just 0 or 1
 unsigned Cdirty[SETS][LINESPERSET];  // just for write-back, 0 or 1 
 unsigned Clru[SETS][LINESPERSET];    // just 0 or 1
 unsigned Ctag[SETS][LINESPERSET];
-//unsigned Cdata[SETS][LINESPERSET][BLOCKSIZE]; // data - not used in this simulation
 
 
 /* counters for statistics */
 int R, memR, hitR; // read ops,  mem reads,  cache hits
 int W, memW, hitW; // write ops, mem writes, cache hits
 
-
-
-
-
-
 /* find tag in a cache set */
 int findInSet( int set, int tag ) {
+
 	int i;
 	for(i = 0; i < LINESPERSET; i++) {
-		if(Ctag[set][i] == tag) {
-			return i;
+		if(Cvalid[set][i] == 1) {
+			if(Ctag[set][i] == tag) {
+				return i;
+			}	
 		}
 	}
 	return -1; // not found
-	
-	
-	
 }
 
 /* replace a cache line with new block read from mem */
+
 void cacheUpdate( int set, int line, int tag ) {
+
 	Cvalid[set][line]=1;   // valid entry
 	Ctag[set][line] = tag; // tag
 	Clru[set][line] = 1;   // accessed
 	Cdirty[set][line] = 0; // not writen (just for write-back)
-	// Cdata[set][line] <- mem block. Not used in this simulation
+	
 }
+
 
 /* simulate one memory access */
 void cacheAccess( unsigned int addr, char op ) {
-	// TODO
+
 	int block = 0;
-	int offset = 0;
+	//int offset = 0;
 	int set = 0;
 	int tag = 0;
 	int line = 0;
 	
-	offset = addr % BLOCKSIZE;
+	//offset = addr % BLOCKSIZE;
 	block = addr / BLOCKSIZE;
+	
 	set = block % SETS;
 	tag = block / SETS;
 
 	line = findInSet( set, tag );
-
+ 
 	if ( line == -1 ) { // MISS
-	
 		
-	
 		int j, k;
 		int found = 0;
 		
-		for(j = 0; j < SETS; j++) {
-			for(k = 0; k < LINESPERSET; k++) {
-				if(Cvalid[j][k] == 0) {
-					cacheUpdate(j, k, tag );
-					found = 1;
+		memR++;
+		
+	
+		for(k = 0; k < LINESPERSET && found == 0; k++) {
+		
+			if(Cvalid[set][k] == 0) {
+				
+				cacheUpdate(set, k, tag );
+				found = 1; 
+				
+				if(op == 'W') {
+					Cdirty[set][k] = 1;
 				}
+				
 			}
 		}
+		
 		
 		if(found == 0) {
-			for(j = 0; j < SETS; j++) {
-				for(k = 0; k < LINESPERSET; k++) {
-					if(Clru[j][k] == 0) {
-						cacheUpdate(j, k, tag );
+		
+			for(j = 0; j < LINESPERSET && found == 0; j++) {
+			
+					if(Clru[set][j] == 0) {
+					
+						if(Cdirty[set][j] == 1 ) {
+							memW++;
+						}
+						
+						cacheUpdate(set, j, tag );
+						found = 1;
+						
+						if(op == 'W'){ 
+							Cdirty[set][j] = 1;
+						}
+							
+						
+							
 					}
 				}
-			}
 		}
+		
 			
-
 		
 	}
+	
 	else { // HIT
-		// TODO
+		
+		Clru[set][line] = 1;
+		
+		
 		if(op == 'W') {
+			Cdirty[set][line] = 1;
 			hitW++;
 		} else {
 			hitR++;
 		}
 
 	}
-	if ( op == 'W' )  // if write-through always writes to mem
-		memW++;
-
-	// TODO
+	
 	int l,m;
 	int found = 0;
 	
-	for(l = 0; l < SETS; l++) {
-		for(m = 0; m < LINESPERSET; m++) {
-			if(Clru[l][m] == 0) {
-				found = 1;
-			}
+	for(m = 0; m < LINESPERSET; m++) {
+		if(Clru[set][m] == 0) {
+			found = 1;
 		}
 	}
 	
+	
 	if(found == 0) {
-		for(l = 0; l < SETS; l++) {
-			for(m = 0; m < LINESPERSET; m++) {
-				Clru[l][m] = 0;
-			}
+		
+		for(l = 0; l < LINESPERSET; l++) {
+			Clru[set][l] = 0;
 		}
+	
 	}
 	
 }
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* print access statistics DON'T CHANGE */
+
 void cache_printstats(void) {
     printf("%d bytes in cache as %d sets, %d lines per set with %d bytes (block size)\n",
 				CACHESIZE, SETS, LINESPERSET, BLOCKSIZE);
@@ -147,6 +217,7 @@ void cache_printstats(void) {
 	printf("Total:  %7d, %7d hits (%.1f%%)\n", R+W, hitR+hitW, 100.0*((double)(hitR+hitW))/(R+W));
 	printf("MemR: %d, MemW: %d (total %d, %.1f%%)\n",
 				memR, memW, memR+memW, 100.0*((double)(memR+memW))/(R+W));
+				
 }
 
 /*
